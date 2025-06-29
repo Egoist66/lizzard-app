400acae053f58b74e95aeda16937e7d685c5a3cb
Farid Mahmudov <razormad666@gmail.com>
2025-06-29 21:06:43 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import TheMenu from './components/TheMenu.vue'
+import { useScoreStore } from './stores/score.store';
 
-
+useScoreStore()
 </script>
 
 
 <template>
   <main class="game">
     <div class="page">
-      <RouterView />
+      <RouterView/>
+
     </div>
+
     <TheMenu />
+
   </main>
 </template>
--- src/stores/score.store.ts
+++ src/stores/score.store.ts
@@ -1,7 +1,33 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
+import { shallowRef } from "vue";
 
-export const useScoreStore = defineStore('score', {
-  state: () => ({
-    score: 0,
-  }),
-})
+export const useScoreStore = defineStore("score", () => {
+  const baseLevelScore = shallowRef<number>(25);
+  const levels = shallowRef<number[]>(
+    new Array(15).fill(0).map((_ , i) => baseLevelScore.value * Math.pow(2, i))
+  );
+
+
+  const levelScores = shallowRef<number[]>(levels.value.map((_, level) => {
+    let sum = 0;
+
+    for(let [index, value] of levels.value.entries()){
+      if(index >= level){
+        return sum + value;
+      }
+      else {
+        sum += value;
+      } 
+    }
+
+    return sum;
+  }));
+
+  return {
+    levels,
+    levelScores,
+    baseLevelScore,
+  };
+});
+
+
